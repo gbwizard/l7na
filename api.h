2e1255b17d55a677351f08b2aee5f64df490053e
Anatoly Nikiforov <Anatoly.Nikiforov@infowatch.com>
2015-10-18 20:43:41 +0300

    * api.h: Добавил основные вызовы;

--- api.h
+++ api.h
@@ -4,29 +4,66 @@ extern "C" {
 
 /*! @brief API системы управления двигателями метеорологической антенны ДМРЛ-3
  *
- *  @attention Методы _не- потокобезопасны.
+ *  Описание работы с системой управления.
+ *
+ *
+ *
+ *  @attention Методы _НЕ_ потокобезопасны.
  */
 
+//! @brief Возможные состояния системы управления.
 enum State {
     STATE_OFF = 0,
     STATE_READY,
     STATE_SCAN_ON_ELEVATION,
     STATE_SCAN_ON_AZIMUTH,
     STATE_POSITION,
-    STATE_ERROR
+    STATE_ERROR,
+    STATE_ERROR_FATAL
 };
 
-//! @brief Инициализируем систему управления.
-//! @return Одно из значений из перечисление State.
-int Init();
+//! @brief Параметры, которые возможно получить из системы управления.
+struct System {
+    struct AxisParams {
+        enum OperationMode {
 
-//! @brief Получаем текущее состояние системы управления.
-//! @return Одно из значений из перечисление State.
-int GetState();
+        };
+
+        double target_velocity;
+        double cur_velocity;
+
+        double tartet_
+    };
+
+    double azimuth_velocity;
+    double azimuth_position;
+    double elevation_velocity;
+    double elevation_position;
+};
+
+//! @brief Инициализирует систему управления.
+//!
+//! @param   cfg_file_path  Путь к файлу с конфигурацией системы (абсолютный или относительно текущей рабочей директории)
+//!
+//! @return Одно из значений в перечислении State.
+int Init(const char* cfg_file_path);
 
 //! @brief Приводит систему управления в первоначальное состояние/выключает систему управления.
-//! @return Одно из значений из перечисление State.
-int Reset();
+//! @return Одно из значений в перечисления State.
+int Release();
+
+int SetModeAzimuthRotation(double elevation_angle, double azimuth_velocity);
+int SetModeElevationRotation(double azimuth_angle, double elevation_velocity);
+int SetModePointPosition(double azimuth_angle, double elevation_angle);
+
+//! @brief Получаем текущее состояние системы управления.
+//! @return Одно из значений в перечислении State.
+int GetState();
+
+double GetAzimuthVelocity();
+double GetAzimuthPosition();
+double GetElevationVelocity();
+double GetElevationPosition();
 
 #ifdef __cplusplus
 } // extern "C"
