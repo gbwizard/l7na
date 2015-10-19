5062eee12f6775395d3c789d062052a142add5cd
Anatoly Nikiforov (aprockiy-ka.rmt.ru) <ace.nikiforov@gmail.com>
2015-10-19 04:26:15 +0300

    * main: Научился работать в Profile position mode и profile velocity mode;

--- main.c
+++ main.c
@@ -24,17 +24,28 @@ static uint32_t gkOffOPos;
 static uint32_t gkOffIStatus;
 static uint32_t gkOffIPos;
 static uint32_t gkOffIVel;
+static uint32_t gkOffPVel;
+static uint32_t gkOffPAccel;
 static uint32_t gkOffITorq;
 static uint32_t gkOffOVel;
+static uint32_t gkOffDVel, gkOffTVel;
+static uint32_t gkOffDPos;
+static uint32_t gkOffOMode, gkOffIMode;
+static uint32_t gkOffPNumer, gkOffPDenom;
 
 const static ec_pdo_entry_reg_t gkDomain1Regs[] = {
-    {0, 1, 0x00007595, 0x00000000, 0x6040, 0, &gkOffOControl},
-//    {0, 1, 0x00007595, 0x00000000, 0x607a, 0, &gkOffOPos},
-    {0, 1, 0x00007595, 0x00000000, 0x6041, 0, &gkOffIStatus},
-    {0, 1, 0x00007595, 0x00000000, 0x6064, 0, &gkOffIPos},
-//    {0, 1, 0x00007595, 0x00000000, 0x606c, 0, &gkOffIVel},
-//    {0, 1, 0x00007595, 0x00000000, 0x6077, 0, &gkOffITorq},
-    {0, 1, 0x00007595, 0x00000000, 0x60FF, 0, &gkOffOVel},
+    {0, 0, 0x00007595, 0x00000000, 0x6040, 0, &gkOffOControl},
+    {0, 0, 0x00007595, 0x00000000, 0x607a, 0, &gkOffOPos},
+    {0, 0, 0x00007595, 0x00000000, 0x6041, 0, &gkOffIStatus},
+    {0, 0, 0x00007595, 0x00000000, 0x6062, 0, &gkOffDPos},
+    {0, 0, 0x00007595, 0x00000000, 0x6064, 0, &gkOffIPos},
+    {0, 0, 0x00007595, 0x00000000, 0x606b, 0, &gkOffDVel},
+    {0, 0, 0x00007595, 0x00000000, 0x606c, 0, &gkOffIVel},
+    {0, 0, 0x00007595, 0x00000000, 0x6081, 0, &gkOffPVel},
+    {0, 0, 0x00007595, 0x00000000, 0x6060, 0, &gkOffOMode},
+    {0, 0, 0x00007595, 0x00000000, 0x6061, 0, &gkOffIMode},
+    {0, 0, 0x00007595, 0x00000000, 0x60ff, 0, &gkOffTVel},
+//    {0, 0, 0x00007595, 0x00000000, 0x200F, 0, &gkOffPDenom},
     {}
 };
 
@@ -125,29 +136,32 @@ int main(int argc, char **argv)
         {0x6041, 0, 16},    // Statusword
         {0x6061, 0, 8},     // The Modes of Operation Display
         {0x6062, 0, 32},    // The Position Demand Value
-        {0x6063, 0, 32},    // The Position Actual Internal Value
         {0x6064, 0, 32},    // The Position Actual Value
         {0x606B, 0, 32},    // The Velocity Demand Value
+        {0x6081, 0, 32},    // The Profile Velocity
         {0x606C, 0, 32},    // The Actual Velocity Value
-        {0x2600, 0, 16},    // The Current Speed (RPM)
-        {0x2601, 0, 16},    // The Command Speed (RPM)
+        {0x607A, 0, 32},    // The Target Position
+//        {0x200E, 0, 16},    // Position Scale Numerator
+//        {0x200F, 0, 16},    // Position Scale Denominator
     };
 
     ec_pdo_info_t l7na_tx_pdos[] = {
-        {0x1A00, 9, l7na_tx_channel1}
+        {0x1A00, 8, l7na_tx_channel1}
     };
 
     // RxPDO
     ec_pdo_entry_info_t l7na_rx_channel1[] = {
-        {0x6041, 0, 16},    // Controlword
+        {0x6040, 0, 16},    // Controlword
         {0x6060, 0, 8},     // Modes of Operation
         {0x607A, 0, 32},    // The Target Position
+        {0x606C, 0, 32},    // The Velocity Demand value
+        {0x6081, 0, 32},    // The Profile Velocity
         {0x60FF, 0, 32},    // The Target Velocity (in Profile Velocity (Pv) mode and Cyclic Synchronous Velocity (Csv) modes)
         {0x6071, 0, 16},    // The Target Torque
     };
 
     ec_pdo_info_t l7na_rx_pdos[] = {
-        {0x1600, 5, l7na_rx_channel1}
+        {0x1600, 7, l7na_rx_channel1}
     };
 
     // Конфигурация SyncManagers 2 (FMMU0) и 3 (FMMU1)
@@ -188,6 +202,8 @@ int main(int argc, char **argv)
 
     fprintf(stdout, "7. Domain data registered.\n");
 
+//goto end;
+
     check_master_state();
     check_domain1_state();
 
@@ -225,13 +241,13 @@ int main(int argc, char **argv)
 
         //перейти в позицию
         const int cmdpos = atoi(argv[1]);
-        uint32_t cmd, val;
         printf("cmd pos: %d\n", cmdpos);
 
         ecrt_master_receive(gkMaster);
         ecrt_domain_process(gkDomain1);
-        cmd = 15;
-        EC_WRITE_U16(gkDomain1PD + gkOffOControl, cmd); //0x6040 ControlWord
+        EC_WRITE_U16(gkDomain1PD + gkOffOControl, 0xF); //0x6040 ControlWord
+        EC_WRITE_U8(gkDomain1PD + gkOffOMode, 3); // 0x6060 Profile position mode
+        EC_WRITE_S32(gkDomain1PD + gkOffTVel, cmdpos); // 0x60ff profile velocity
         ecrt_domain_queue(gkDomain1);
         ecrt_master_send(gkMaster);
         usleep(1000);
@@ -248,8 +264,8 @@ int main(int argc, char **argv)
 
         ecrt_master_receive(gkMaster);
         ecrt_domain_process(gkDomain1);
-        val = cmdpos;
-        EC_WRITE_U32(gkDomain1PD + gkOffOVel, val);
+//        EC_WRITE_S32(gkDomain1PD + gkOffOPos, cmdpos);
+//        EC_WRITE_U16(gkDomain1PD + gkOffOControl, 0x1F);
         ecrt_domain_queue(gkDomain1);
         ecrt_master_send(gkMaster);
         usleep(1000);
@@ -265,44 +281,63 @@ int main(int argc, char **argv)
 
 /*        ecrt_master_receive(gkMaster);
         ecrt_domain_process(gkDomain1);
-        cmd = 31;
-        EC_WRITE_U16(gkDomain1PD + gkOffOControl, cmd);
-        //EC_WRITE_U32(gkDomain1PD + gkOffOPos, val);
+        EC_WRITE_S32(gkDomain1PD + gkOffOPos, cmdpos);
         ecrt_domain_queue(gkDomain1);
         ecrt_master_send(gkMaster);
-        usleep(1000);
-*/
+        usleep(1000);*/
+
         //wait
-        for (uint32_t i = 0; i < 1000; ++i) {
+/*        for (uint32_t i = 0; i < 1000; ++i) {
             ecrt_master_receive(gkMaster);
             ecrt_domain_process(gkDomain1);
             ecrt_domain_queue(gkDomain1);
             ecrt_master_send(gkMaster);
             usleep(1000);
         }
+*/
 
 #if 1
         for (uint32_t j = 0; ; j++) {
            ecrt_master_receive(gkMaster);
            ecrt_domain_process(gkDomain1);
-           uint32_t ipos_new = EC_READ_U32(gkDomain1PD + gkOffIPos); //READ DATA 0x6064 position
-           uint32_t istatus_new = EC_READ_U16(gkDomain1PD + gkOffIStatus); //READ DATA 0x6041 status
+           int32_t ipos_new = EC_READ_S32(gkDomain1PD + gkOffIPos); //READ DATA 0x6064 position
+           uint16_t istatus_new = EC_READ_U16(gkDomain1PD + gkOffIStatus); //READ DATA 0x6041 status
+            int32_t imode = EC_READ_S8(gkDomain1PD + gkOffIMode);
+            int32_t ipvel = EC_READ_S32(gkDomain1PD + gkOffPVel);
+            int32_t idvel = EC_READ_S32(gkDomain1PD + gkOffDVel);
+            int32_t iavel = EC_READ_S32(gkDomain1PD + gkOffIVel);
+            int32_t idpos = EC_READ_S32(gkDomain1PD + gkOffDPos);
+            int32_t itpos = EC_READ_S32(gkDomain1PD + gkOffOPos);
+            int32_t icontrol = EC_READ_U16(gkDomain1PD + gkOffOControl);
+//            int32_t ipdenom = EC_READ_S16(gkDomain1PD + gkOffPDenom);
             if (ipos_new != ipos || istatus_new != istatus) {
                 ipos = ipos_new;
                 istatus = istatus_new;
-                printf("Position: %d Status: 0x%x\n", ipos, istatus);
+                printf("Position: %d Status: 0x%x Mode: %d PVel: %d DVel: %d AVel: %d DPos: %d TPos: %d OControl: 0x%x\n", ipos, istatus, imode, ipvel, idvel, iavel, idpos, itpos, icontrol);
             }
-//          ecrt_domain_queue(gkDomain1);
-//          ecrt_master_send(gkMaster);
+
+            if((istatus_new >> 10) & 0x1) {
+               printf("Target reached. Pos: %d Status: 0x%x\n", ipos, istatus);
+           }
+
+        if (j == 10000) {
+            printf("Iterations=%d, stopping",j);
+            EC_WRITE_U16(gkDomain1PD + gkOffOControl, 0x6);
+            break;
+        }
+
+            
+          ecrt_domain_queue(gkDomain1);
+          ecrt_master_send(gkMaster);
            usleep(1000); //WAIT 1mS
 
-           if(ipos == cmdpos) {
-               printf("2-Position: %x Status: %x\n", ipos, istatus);
-               break;
-           }
         }
 #endif
     }
+
+    end:
+
+    
 
     ecrt_master_receive(gkMaster);
     ecrt_domain_process(gkDomain1);
